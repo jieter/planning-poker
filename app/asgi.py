4da8ddd6f09b98669f2b1d5b5253cb8ce3d72b24
Jan Pieter Waagmeester <jieter@waarneming.nl>
2023-03-12 22:03:44 +0100

    prevent apps AppRegistryNotReady: Apps aren't loaded yet.

--- app/asgi.py
+++ app/asgi.py
@@ -7,12 +7,13 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
+django_asgi_app = get_asgi_application()
 import app.poker.routing
 
 router = SessionMiddlewareStack(URLRouter(app.poker.routing.websocket_urlpatterns))
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": router,
         # "websocket": AllowedHostsOriginValidator(router),
     }
