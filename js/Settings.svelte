9282d5d784ac9406a1049c02c87a73b1137377fa
Jan Pieter Waagmeester <jieter@waarneming.nl>
2023-06-04 16:36:47 +0200

    Do not allow changing decks when not revealed

--- js/Settings.svelte
+++ js/Settings.svelte
@@ -1,19 +1,16 @@
 <script>
-import Toggle from './Toggle.svelte';
-
-import { decks, deck, autoReveal } from './stores.js';
+import { autoReveal, deck, decks, isRevealed } from './stores.js';
 </script>
-
-Deck:
 
 <div class="btn-group btn-group-sm" role="group" aria-label="Change deck">
     {#each $decks as [value, label]}
-        <input type="radio" class="btn-check" {value} bind:group={$deck} autocomplete="off" id="deck-{value}" />
+        <input type="radio" class="btn-check" {value} bind:group={$deck} autocomplete="off" id="deck-{value}" disabled={!$isRevealed}/>
         <label class="btn btn-outline-primary" for="deck-{value}"> {label}</label>
     {/each}
 </div>
 <br />
-<div class="form-check form-switch">
+
+<div class="form-check form-switch mt-2">
     <label for="autoReveal">Auto reveal</label>
     <input type="checkbox" bind:checked={$autoReveal} class="form-check-input" id="autoReveal" />
 </div>
