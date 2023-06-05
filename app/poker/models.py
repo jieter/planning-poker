f203a01005c6a9f409284e999564dc0a2cccac84
Jan Pieter Waagmeester <jieter@waarneming.nl>
2023-06-05 19:48:11 +0200

    Do not count spectators for is_voting_complete
    fixes: #12

--- app/poker/models.py
+++ app/poker/models.py
@@ -48,7 +48,7 @@ class PokerSession(models.Model):
 
     @property
     def is_voting_complete(self):
-        return self.active_users.filter(vote__isnull=True).count() == 0
+        return self.active_users.exclude(is_spectator=True).filter(vote__isnull=True).count() == 0
 
     def clear(self) -> None:
         """Clear all votes and return to voting state."""
