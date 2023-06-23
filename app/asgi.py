f90d4cced74bd963f54b7c121badfd2b5a4413f4
Jan Pieter Waagmeester <jieter@waarneming.nl>
2023-06-23 10:52:54 +0200

    Adopt ruff

--- app/asgi.py
+++ app/asgi.py
@@ -1,20 +1,19 @@
 import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from channels.sessions import SessionMiddlewareStack
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 django_asgi_app = get_asgi_application()
-import app.poker.routing
+
+import app.poker.routing  # noqa
 
 router = SessionMiddlewareStack(URLRouter(app.poker.routing.websocket_urlpatterns))
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
         "websocket": router,
-        # "websocket": AllowedHostsOriginValidator(router),
     }
 )
