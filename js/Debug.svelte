2dcd9c7a516d4c944315a03d43773c044ddf661f
Jan Pieter Waagmeester <jieter@waarneming.nl>
2023-07-28 14:00:04 +0200

    Small cleanups in debug component

--- js/Debug.svelte
+++ js/Debug.svelte
@@ -7,16 +7,12 @@ const isProduction = !window.location.host.includes('localhost');
 </script>
 
 <div class="bg-light rounded p-2">
-    <div>debug</div>
-    <code>{JSON.stringify(settings)}</code>
+    <div>debug<code>{JSON.stringify(settings)}</code></div>
     <div>votes: <code>{JSON.stringify($votes)}</code></div>
     <div>user: <code>{JSON.stringify($user)}</code></div>
     <div>participants: <code>{JSON.stringify($participants)}</code></div>
     {#if !isProduction}
-        Fake:
-        <div class="btn-group btn-group-sm" role="group">
-            <button on:click={() => update('add_fakes')} class="btn btn-warning">Users</button>
-            <button on:click={() => update('fake_votes')} class="btn btn-warning">Votes</button>
-        </div>
+        <button on:click={() => update('add_fakes')} class="btn btn-warning">Add fake users</button>
+        <button on:click={() => update('fake_votes')} class="btn btn-warning">Add fake votes</button>
     {/if}
 </div>
