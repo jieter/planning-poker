5870cd55d291a2cb3d3af36823312cb01fd7ed9c
Jan Pieter Waagmeester <jieter@waarneming.nl>
2023-10-17 07:42:36 +0200

    Factor out vote counting

--- js/History.svelte
+++ js/History.svelte
@@ -1,20 +1,10 @@
 <script>
-import { log } from './stores.js';
+import { log, countVotes } from './stores.js';
 import Summary from './Summary.svelte';
-
-function summary(votes) {
-    const _votes = new Proxy({}, { get: (d, key) => (key in d ? d[key] : 0) });
-    votes.forEach((vote) => {
-        if (vote != null) {
-            _votes[vote] += 1;
-        }
-    });
-    return Object.entries(_votes).sort((a, b) => b[1] - a[1]);
-}
 </script>
 
 {#each $log.filter((x) => x.event == 'reveal') as { data }}
-    {@const voteSummary = summary(data.votes)}
+    {@const voteSummary = countVotes(data.votes)}
     <div class="small">
         Round {data.round}:
         {#if voteSummary.length == 0}
