a2fa0bc3033011a51621b3b17cfec31b855636fa
Jan Pieter Waagmeester <jieter@waarneming.nl>
2024-01-10 11:03:48 +0100

    Fix websocket url

--- js/App.svelte
+++ js/App.svelte
@@ -1,8 +1,9 @@
 <script>
 import Join from './Join.svelte';
 import Voting from './Voting.svelte';
+import { jsonScriptContents } from './utils';
 
-export let websocketUrl;
+export let websocketUrl = jsonScriptContents('websocket_url');;
 </script>
 
 {#if websocketUrl}
