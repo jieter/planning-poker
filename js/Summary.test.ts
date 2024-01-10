f3263904243c7a8d2569999dc1208dccd8931126
Jan Pieter Waagmeester <jieter@waarneming.nl>
2024-01-10 13:39:00 +0100

    Add vitest
    refs: https://blog.logrocket.com/testing-svelte-app-vitest/

---
+++ js/Summary.test.ts
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/svelte";
+import { describe, expect, test } from 'vitest'
+
+import Summary from "./Summary.svelte";
+
+describe('Summary.svelte', () => {
+    test('no votes', () => {
+        render(Summary, {props: {votes: []}});
+
+        expect(screen.getByText('No votes')).toBeInTheDocument()
+    });
+});
