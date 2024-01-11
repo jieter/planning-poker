abca6a16789031d0692353ae3eeb2250d209e36d
Jan Pieter Waagmeester <jieter@waarneming.nl>
2024-01-11 15:56:05 +0100

    Extend test even more

--- js/utils.test.ts
+++ js/utils.test.ts
@@ -1,6 +1,19 @@
 import { describe, expect, test } from 'vitest';
-import { countVotes, pseudoRandomGenerator } from './utils';
+import { countVotes, formatNumber, pseudoRandomGenerator } from './utils';
 
+describe('formatNumber', () => {
+    test('less than 1000', () => {
+        expect(formatNumber(1)).toEqual('1');
+        expect(formatNumber(999)).toEqual('999');
+    });
+    test('1000 and above', () => {
+        expect(formatNumber(1000)).toEqual('1k');
+        expect(formatNumber(2000)).toEqual('2k');
+        expect(formatNumber(2020)).toEqual('2k');
+        expect(formatNumber(2200)).toEqual('2.2k');
+        expect(formatNumber(10000)).toEqual('10k');
+    });
+});
 describe('pseudoRandomGenerator', () => {
     test('with the same seed, output is predictable', () => {
         const generator = pseudoRandomGenerator(1, 1, 10);
@@ -16,5 +29,16 @@ describe('pseudoRandomGenerator', () => {
 describe('countVotes', () => {
     test('no votes', () => {
         expect(countVotes([])).toEqual([]);
+    });
+    test('with votes', () => {
+        expect(countVotes(['1', '1'])).toEqual([['1', 2]]);
+        expect(countVotes(['1', '2', '1'])).toEqual([
+            ['1', 2],
+            ['2', 1],
+        ]);
+        expect(countVotes(['XL', 'XL', 'XL', 'L'])).toEqual([
+            ['XL', 3],
+            ['L', 1],
+        ]);
     });
 });
